2f3e546bbd1020fcd7da37c6e0c1cf27ae48bca2
Sadisu Rabiu Khamis <khaleepha2010@gmail.com>
2023-05-18 12:34:55 +0100

    commit2

---
+++ problem2_2.py
@@ -0,0 +1,15 @@
+
+def problem2_2(l):
+	print(l)
+	print(l[0])
+	print(l[-1])
+	print(l[3:5])
+	print(l[0:3])
+	print(l[3:])
+	print(len(l))
+	l.append('z')
+	print(l)
+	
+alist = ["a","e","i","o","u","y"]
+blist = ["alpha", "beta", "gamma","delta", "epsilon", "eta", "theta"]
+     
